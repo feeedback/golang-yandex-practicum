82c94a0e35ab521ea31984cd2ef5c44cb350cc38
Alexander Rzhanitsyn <risdeep@gmail.com>
2023-09-05 00:55:34 +0400

    refactor chapter_2_4\lesson_2

--- chapter_2_4/lesson_2/8/main.go
+++ chapter_2_4/lesson_2/8/main.go
@@ -1,18 +1,22 @@
 package main // Тема 1/4: Композитные типы → Урок 2/5
 // https://practicum.yandex.ru/trainer/go-basics/lesson/57d4b5d3-6df9-4653-a875-e83f47538e7a/
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
-	input := "The quick brown 狐 jumped over the lazy 犬"
+	input := "Привет, мир!"
 
 	runes := []rune(input)
-	n := len(runes)
 
-	for i := 0; i < n/2; i++ {
-		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
-	}
+	// // before Go 1.21.0
+	// n := len(runes)
+	// for i := 0; i < n/2; i++ {
+	// 	runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	// }
+	slices.Reverse(runes)
 
-	str := string(runes)
-	fmt.Println(str)
+	fmt.Println(string(runes))
 }
